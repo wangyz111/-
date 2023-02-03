fa8b46eba844cb9057bcb2c9fb31eb108e4ef787
wangyz111 <wang137710>
2023-02-03 17:16:47 +0800

    1111

---
+++ untitled/src/main/java/HiWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @program: demo2
+ * @description:
+ * @author: wyz
+ * @create: 2023-02-03 17:15
+ **/
+public class HiWorld {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("1111111111111");
+    }
+}
